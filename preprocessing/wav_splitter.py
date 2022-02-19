fac33a1b51d3408d73f32c156c2115510f37f672
t-thor <tjthorsen@ucdavis.edu>
2022-02-19 14:09:27 -0800

    no splitting (temporary)

--- preprocessing/wav_splitter.py
+++ preprocessing/wav_splitter.py
@@ -5,20 +5,21 @@ def wav_split(filename):
     audio = AudioSegment.from_wav(filename)
     if audio.duration_seconds < 29:
         pass
-    elif audio.duration_seconds <= 60:
+    else:#if audio.duration_seconds <= 60:
         split_audio = audio
         split_audio.export('output/audio/splitted{}.wav', format="wav")
-    else:
-        total_ms = len(audio)
-        num_seg = int(total_ms / 30000) 
-        if num_seg > 3:     ##temp fix
-            num_seg = 3 
+    
+    #else:
+    #    total_ms = len(audio)
+    #    num_seg = int(total_ms / 30000) 
+    #    if num_seg > 3:     ##temp fix
+    #        num_seg = 3 
 
-        for i in range(num_seg):
-            start = i * 30000 #random.randint(5000, total_ms - 35000) # exclude first 5 seconds and last 5 seconds
-            end = start + 30000 # take 30 seconds
-            split_audio = audio[start:end]
-            split_audio.export('output/audio/splitted{}.wav'.format(i), format="wav")
+#        for i in range(num_seg):
+#            start = i * 30000 #random.randint(5000, total_ms - 35000) # exclude first 5 seconds and last 5 seconds
+#            end = start + 30000 # take 30 seconds
+#            split_audio = audio[start:end]
+#            split_audio.export('output/audio/splitted{}.wav'.format(i), format="wav")
         
 #def to3_split(filename):
 #    audio = AudioSegment.from_wav(filename)
