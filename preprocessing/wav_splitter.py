34ff07346bd0fd921a6926ea9740118f4b50e4bd
t-thor <tjthorsen@ucdavis.edu>
2022-02-20 13:34:30 -0800

    test 24 samples

--- preprocessing/wav_splitter.py
+++ preprocessing/wav_splitter.py
@@ -9,7 +9,7 @@ def wav_split(filename):
     else:
         total_ms = len(audio)
 
-        for i in range(5):
+        for i in range(24):
             start = random.randint(5000, total_ms - 35000) # exclude first 5 seconds and last 5 seconds
             end = start + 30000 # take 30 seconds
             split_audio = audio[start:end]
