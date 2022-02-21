740ce07727ffc1a10fb966bea4c07177bfc72385
t-thor <tjthorsen@ucdavis.edu>
2022-02-21 11:27:16 -0800

    +num_samples

--- preprocessing/wav_splitter.py
+++ preprocessing/wav_splitter.py
@@ -1,10 +1,11 @@
 from pydub import AudioSegment
 import random
 
-def wav_split(filename):
+def wav_split(filename, num_samples):
     audio = AudioSegment.from_wav(filename)
+    total_ms = len(audio)
 
-    for i in range(8):
+    for i in range(num_samples):
         start = random.randint(5000, total_ms - 35000) # exclude first 5 seconds and last 5 seconds
         end = start + 30000 # take 30 seconds
         split_audio = audio[start:end]
