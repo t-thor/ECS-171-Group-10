10ff5d429493afb5899029d08fa5b113a24a1245
t-thor <tjthorsen@ucdavis.edu>
2022-02-21 11:17:48 -0800

    added parameter

--- preprocessing/wav_splitter.py
+++ preprocessing/wav_splitter.py
@@ -1,19 +1,14 @@
 from pydub import AudioSegment
 import random
 
-def wav_split(filename):
+def wav_split(filename, samples):
     audio = AudioSegment.from_wav(filename)
-    if audio.duration_seconds < 35:
-        split_audio = audio
-        split_audio.export('output/audio/splitted.wav', format="wav")
-    else:
-        total_ms = len(audio)
 
-        for i in range(8):
-            start = random.randint(5000, total_ms - 35000) # exclude first 5 seconds and last 5 seconds
-            end = start + 30000 # take 30 seconds
-            split_audio = audio[start:end]
-            split_audio.export('output/audio/splitted{}.wav'.format(i), format="wav")
+    for i in range(samples):
+        start = random.randint(5000, total_ms - 35000) # exclude first 5 seconds and last 5 seconds
+        end = start + 30000 # take 30 seconds
+        split_audio = audio[start:end]
+        split_audio.export('output/audio/splitted{}.wav'.format(i), format="wav")
         
 #def to3_split(filename):
 #    audio = AudioSegment.from_wav(filename)
