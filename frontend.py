9eea54b2fd84971d51ffa80709fe4a295dbf43a8
t-thor <tjthorsen@ucdavis.edu>
2022-02-21 20:24:09 -0800

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -22,9 +22,8 @@ audio_path = os.path.dirname(__file__) + '/output/audio/'
 st.write("""
 # Music Genre Classification
 ## ECS171 Group 8 Final Project
-## University of California, Davis
-This app analyzes a song and classifies it into one of the 10 genres
-It was trained using the GTZAN dataset, which includes 30 second song clips from a variety of genres which were classified by experts
+### University of California, Davis
+This app analyzes a song and classifies it into one of the 10 genres. It was trained using the GTZAN dataset, which includes 30 second song clips from a variety of genres which were classified by experts
 
 Because the songlist was assembled in 2002, the model is less accurate when attempting to describe the genre of more recent songs. 
 
